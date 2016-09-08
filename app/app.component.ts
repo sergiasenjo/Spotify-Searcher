2b1f05387a8d42596ef15a5683d3430f8935cdde
Sergio Asenjo <sergio.asenjo@novaquality.es>
2016-09-08 17:26:07 +0200

    Navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: '<navbar></navbar>'
+    templateUrl: 'app.component.html'
 })
 export class AppComponent { }
