cdc714f065d6fd0e193dfb29c773aad822933f19
SergioC <sergiasenjo@gmail.com>
2016-09-08 23:00:40 +0200

    Search component and Spotify service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,21 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
-import { NavbarComponent }  from './components/navbar/navbar.component';
-import { AboutComponent }  from './components/about/about.component';
-import { SearchComponent }  from './components/search/search.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+
+import { SpotifyService } from './services/spotify.service';
 
 import { routing, appRoutingProviders } from './app.routing';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [ BrowserModule, routing ],
+  imports: [ BrowserModule, routing, HttpModule, FormsModule ],
   declarations: [ AppComponent, NavbarComponent, AboutComponent, SearchComponent ],
-  providers: [ appRoutingProviders ],
+  providers: [ appRoutingProviders, SpotifyService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
